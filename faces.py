28ed75a679f1d412fbe38a8bd5cc1994a0a1629f
root <brent.mcmanus@me.com>
2018-09-10 21:28:31 -0700

    basic test file

---
+++ faces.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    #capture frame by frame
+    ret,frame = cap.read()
+
+    #display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+#when complete
+cap.release()
+cv2.destroyAllWindows()
